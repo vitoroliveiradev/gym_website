5df2f332be6cfaaad1fcc82f08e28f695e2ef1bc
vitoroliveiradev <melooliveira2003@gmail.com>
2022-07-29 11:46:03 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { Hero } from './components/Hero/Hero';
+import { Plans } from './components/Plans/Plans';
+import { Programs } from './components/Programs/Programs';
+import { Reasons } from './components/Reasons/Reasons';
+import { Testimonials } from './components/Testimonials/Testimonials';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
+      <Testimonials />
     </div>
   );
 }
---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,37 @@
+import "./Plans.css";
+import { plansData } from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
+
+export const Plans = () => {
+    let arrow = "->"
+  return (
+    <div className="plans-container" style={{ gap: "2rem" }}>
+      <div className="programs-header">
+        <span className="stroke-text">READY TO START</span>
+        <span>YOUR JOURNEY</span>
+        <span className="stroke-text">NOW WITHUS</span>
+      </div>
+      <div className="plans">
+        {plansData.map((plan, index) => (
+          <div className="plan" key={index}>
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>${plan.price}</span>
+            <div className="features">
+              {plan.features.map((feature, index) => (
+                <div className="feature">
+                  <img src={whiteTick} alt="Imagem white tick" />
+                  <span key={index}>{feature}</span>
+                </div>
+              ))}
+            </div>
+            <div>
+              <span>See more benefits {arrow}</span>
+            </div>
+            <button className="btn">Join now</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
